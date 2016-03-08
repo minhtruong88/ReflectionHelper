4b1fcce0dc6e30dcf4be03dfc749dcc406967a48
minhtruong88 <minhtruong017@gmail.com>
2016-03-07 19:57:34 -0800

    Reflection Extension
    This is a helper Extension Library to simplify the usage of reflection
    for set property and get property.
    
    Example:
    Test t = new Test();
    t = t.Set_Property("id", 1);
    Object obj = t.Get_Property("id");

---
+++ ReflectionExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ReflectionHelper
+{
+    public static class ReflectionExtension
+    {
+        public static T Set_Property<T>(this T data, string name, Object value)
+        {
+            foreach (PropertyInfo property in data.GetType().GetProperties())
+            {
+                if (property.Name.ToLower() == name.ToLower())
+                {
+                    property.SetValue(data, value, null);
+                }
+            }
+            return data;
+        }
+
+        public static Object Get_Property<T>(this T data, string name)
+        {
+            Object result = null;
+            foreach (PropertyInfo property in data.GetType().GetProperties())
+            {
+                if (property.Name.ToLower() == name.ToLower())
+                {
+                    result = property.GetValue(data, null);
+                }
+            }
+            return result;
+        }
+    }
+}
